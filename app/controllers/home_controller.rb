16bf4d878b1e1cb7a4bacdb4286bc7b444004e20
Gustavo Varallo <gustavo.varallo@live.com>
2014-09-21 21:45:05 -0300

    Add a Static Pages controller

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  def help
-  end
-
-  def about
-  end
-end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
-class HomeControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
   test "should get help" do
     get :help
     assert_response :success
